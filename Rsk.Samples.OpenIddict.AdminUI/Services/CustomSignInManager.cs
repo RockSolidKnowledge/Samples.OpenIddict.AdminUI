aea5177e2b1ff93011e21471bccb138eab3b611d
NachoEscrig <nacho.escrig@rocksolidknowledge.com>
2024-05-22 19:32:54 +0100

    Fix: Find user name to use in authentication instead of email

---
+++ Rsk.Samples.OpenIddict.AdminUI/Services/CustomSignInManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Velusia.Server.Data;
+
+namespace OpenIddict.Sandbox.AspNetCore.Server.Services;
+
+public class CustomSignInManager : SignInManager<ApplicationUser>
+{
+    public CustomSignInManager(UserManager<ApplicationUser> userManager, 
+        IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory, 
+        IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<ApplicationUser>> logger, 
+        IAuthenticationSchemeProvider schemes, IUserConfirmation<ApplicationUser> userConfirmation)
+        : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, userConfirmation)
+    {
+    }
+
+    public override async Task<SignInResult> PasswordSignInAsync(string userName, string password,
+        bool isPersistent, bool lockoutOnFailure)
+    {
+        var user = await UserManager.FindByEmailAsync(userName);
+        if (user == null)
+        {
+            return SignInResult.Failed;
+        }
+
+        return await PasswordSignInAsync(user, password, isPersistent, lockoutOnFailure);
+    }
+}
