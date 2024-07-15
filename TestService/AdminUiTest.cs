294fb225c4b870955be92efbc226e4f6b8e3f4b4
Richard Ngwa <richard.ngwa@rocksolidknowledge.com>
2024-07-15 16:18:39 +0100

    Gave containers clearer names for debugging

--- TestService/AdminUiTest.cs
+++ TestService/AdminUiTest.cs
@@ -27,7 +27,7 @@ namespace TestService
         {
             IPage page = await TestFixture.BrowserContext!.NewPageAsync();
 
-            await page.GotoAsync(AdminUiAutoTestFixture.RunningInDocker ? "http://ui-int:5000" : "https://localhost:5000");
+            await page.GotoAsync(AdminUiAutoTestFixture.RunningInDocker ? "http://admin-ui:5000" : "https://localhost:5000");
 
             return new WelcomePage(page);
         }
--- TestService/start.ps1
+++ TestService/start.ps1
@@ -6,9 +6,16 @@ param(
 
 Copy-Item -Path $SourceFilePath/docker-compose.yml .
 
+# To repeatedly re-run tests locally, after code changes for example, we need to remove any relicks of prior runs (like published folder contents and database objects)
+echo "Removing existing containers ..."
+docker rm $(docker ps -a -q)
+
 docker-compose up --build --exit-code-from ui-tests
 
 #New-Item -ItemType Directory -Force -Path ./Errors
 
-docker-compose logs --no-color ui-int > ./ui-logs.txt
+docker-compose logs --no-color admin-ui > ./ui-logs.txt
 docker-compose logs --no-color ui-tests > ./uitests-logs.txt
+
+echo "Freeing up disk space"
+docker image rm $(docker images -f "dangling=true" -q)
