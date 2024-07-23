6d7710a9aac84fe070601fb76af5d9416926446d
Richard Ngwa <richard.ngwa@rocksolidknowledge.com>
2024-07-23 16:38:13 +0100

    WIP : Testing that Custom ClaimTypes, configured via AdminUI configuration, are listed in [Claim Types] view

--- TestService/Pages/HomePageViews/ClaimTypes/ClaimTypesPage.cs
+++ TestService/Pages/HomePageViews/ClaimTypes/ClaimTypesPage.cs
@@ -12,5 +12,28 @@ namespace TestService.Pages.HomePageViews.ClaimTypes
 
             return new NewClaimTypeSetup(CurrentPage);
         }
+
+        public async Task<bool> ConfirmClaimsListed(List<string> expectedClaimTypes)
+        {
+            if (expectedClaimTypes.Count == 0)
+            {
+                return true;  // We have verified that there are no configured custom claim types in AdminUI appsettings.json
+            }
+
+            var allListedClaimTypes = new List<string>();
+
+            int rowCount = await CurrentPage.Locator(".tbody").Locator("tr").CountAsync();
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                var listedCLaimName = await CurrentPage.Locator(".tbody").Locator("tr").Nth(i).Locator("td").Nth(1).InnerTextAsync();
+                if (expectedClaimTypes.Contains(listedCLaimName))
+                {
+                    expectedClaimTypes.Remove(listedCLaimName);
+                }
+            }
+
+            return expectedClaimTypes.Count == 0;
+        }
     }
 }
---
+++ TestService/TestCollections/HomePageViews/ClaimTypesView/CustomClaimTypes.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using TestService.Models;
+using TestService.Pages;
+using TestService.Pages.HomePageViews.Clients;
+using TestService.TestCollections.Fixture;
+using TestService.TestCollections.HomePageViews.ClientsView;
+using Xunit.Extensions.Ordering;
+using static TestService.TestCollections.HomePageViews.ClientsView.SetupInfoFactory;
+
+namespace TestService.TestCollections.HomePageViews.ClaimTypesView
+{
+    [Order(CollectionRunOrder.AddClaimType)]
+    public class CustomClaimTypes(AdminUiAutoTestFixture testFixture) : AdminUiTest(testFixture)
+    {
+        [Fact, Order(1)]
+        public async Task ConfiguredCustomClaimTypeArePresent()
+        {
+            var configuredClaimTypeNames = await GetCustomAdminUiClaimTypes();
+
+            bool customClaimsAreListed = await LoginToAdminUi(UserFactory.GetTheTestUser().EmailAddress)
+                                        .AndThen(p => p.GotoTheClaimTypesView())
+                                        .AndThen(p => p.ConfirmClaimsListed(configuredClaimTypeNames));
+
+            customClaimsAreListed.Should().BeTrue();
+        }
+
+        private async Task<List<string>> GetCustomAdminUiClaimTypes()
+        {
+            var path = GetAdminUiAppSettingsJson();
+
+            using StreamReader reader = new(path);
+            string jsonString = await reader.ReadToEndAsync();
+
+            var claimTypesJTokens = JObject.Parse(jsonString)["CustomClaimTypes"];
+
+            if (claimTypesJTokens == null)
+            {
+                return new List<string>();
+            }
+
+            return claimTypesJTokens.Select(s => s["Name"].Value<string>()).ToList();
+        }
+
+        private static string GetAdminUiAppSettingsJson()
+        {
+            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+
+            return new(directory!.FullName + "\\AdminUI\\appsettings.json");
+        }
+    }
+}
