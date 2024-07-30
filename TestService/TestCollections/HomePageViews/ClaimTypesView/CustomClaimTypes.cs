4775d9968b91d942a649d4b0d215ae25a9ef8b1b
Richard Ngwa <richard.ngwa@rocksolidknowledge.com>
2024-07-30 15:22:37 +0100

    Refactored to conform with standard flow.

--- TestService/TestCollections/HomePageViews/ClaimTypesView/CustomClaimTypes.cs
+++ TestService/TestCollections/HomePageViews/ClaimTypesView/CustomClaimTypes.cs
@@ -2,11 +2,9 @@
 using Newtonsoft.Json.Linq;
 using TestService.Models;
 using TestService.Pages;
-using TestService.Pages.HomePageViews.Clients;
+using TestService.Pages.HomePageViews.ClaimTypes;
 using TestService.TestCollections.Fixture;
-using TestService.TestCollections.HomePageViews.ClientsView;
 using Xunit.Extensions.Ordering;
-using static TestService.TestCollections.HomePageViews.ClientsView.SetupInfoFactory;
 
 namespace TestService.TestCollections.HomePageViews.ClaimTypesView
 {
@@ -14,46 +12,15 @@ namespace TestService.TestCollections.HomePageViews.ClaimTypesView
     public class CustomClaimTypes(AdminUiAutoTestFixture testFixture) : AdminUiTest(testFixture)
     {
         [Fact, Order(1)]
-        public async Task ConfiguredCustomClaimTypesShouldBeListedInClaimTypesTable()
+        public async Task ClaimTypes_ConfiguredClaimTypesShouldBeListedInClaimTypesTable()
         {
-            //var configuredClaimTypeNames = await GetCustomAdminUiClaimTypes();
             var configuredClaimTypeNames = new List<string>() { "playwright_test1", "playwright_test2" };
 
-            bool customClaimsAreListed = await LoginToAdminUi(UserFactory.GetTheTestUser().EmailAddress)
-                                        .AndThen(p => p.GotoTheClaimTypesView())
-                                        .AndThen(p => p.ConfirmConfiguredClaimTypesAreListed(configuredClaimTypeNames));
+            ClaimTypesPage page = await LoginToAdminUi(UserFactory.GetTheTestUser().EmailAddress)
+                                                        .AndThen(p => p.GotoTheClaimTypesView());
 
-            customClaimsAreListed.Should().BeTrue();
-        }
-
-
-        private async Task<List<string>> GetCustomAdminUiClaimTypes()
-        {
-            var path = GetAdminUiAppSettingsJson();
-
-            using StreamReader reader = new(path);
-            string jsonString = await reader.ReadToEndAsync();
-
-            var claimTypesJTokens = JObject.Parse(jsonString)["CustomClaimTypes"];
-
-            if (claimTypesJTokens == null)
-            {
-                return new List<string>();
-            }
-
-            return claimTypesJTokens.Select(s => s["Name"].Value<string>()).ToList();
-        }
-
-        private static string GetAdminUiAppSettingsJson()
-        {
-            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
-
-            while (directory != null && !directory.GetFiles("*.sln").Any())
-            {
-                directory = directory.Parent;
-            }
-
-            return new(directory!.FullName + "\\AdminUI\\appsettings.json");
+            bool claimsAreListed = await page.ConfirmConfiguredClaimTypesAreListed(configuredClaimTypeNames);
+            claimsAreListed.Should().BeTrue();
         }
     }
 }
