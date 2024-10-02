97e5757c3c7bf2b7a5013c0266e72c24dd959bd8
James Britton <james.britton@rocksolidknowledge.com>
2024-10-02 12:14:29 +0100

    fix: build issues

--- Rsk.Samples.OpenIddict.AdminUI/Program.cs
+++ Rsk.Samples.OpenIddict.AdminUI/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Velusia.Server;
+namespace Rsk.Samples.OpenIddict.AdminUiIntegration;
 
 public static class Program
 {
