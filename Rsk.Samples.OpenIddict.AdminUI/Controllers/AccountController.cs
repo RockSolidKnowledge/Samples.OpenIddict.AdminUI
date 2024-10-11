e66d78024d93ad013d91a166d2e6ca609941ca5d
NachoEscrig <nacho.escrig@rocksolidknowledge.com>
2024-10-11 12:27:32 +0100

    Add login and logout pages

---
+++ Rsk.Samples.OpenIddict.AdminUI/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Rsk.Samples.OpenIddict.AdminUiIntegration.Data;
+using Rsk.Samples.OpenIddict.AdminUiIntegration.Models;
+
+namespace Rsk.Samples.OpenIddict.AdminUiIntegration.Controllers;
+
+public class AccountController(SignInManager<ApplicationUser> signInManager) : Controller
+{
+    [HttpGet]
+    public IActionResult Login([FromQuery] string returnUrl)
+    {
+        return View(new LoginInputModel
+        {
+            ReturnUrl = returnUrl
+        });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginInputModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await signInManager.UserManager.FindByEmailAsync(model.Email);
+            if (user?.UserName == null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Credentials");
+                return View(model);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Credentials");
+                return View(model);
+            }
+
+            await signInManager.SignInAsync(user, false);
+
+            var returnUrl = model.ReturnUrl;
+            if (returnUrl == null || !Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect("~/");
+            }
+
+            return Redirect(returnUrl);
+        }
+        
+        ModelState.AddModelError(string.Empty, "Invalid username or password");
+        
+        return View(model);
+    }
+    
+    [HttpGet]
+    public IActionResult Logout()
+    {
+        return View(new LogoutViewModel());
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout(LogoutInputModel model)
+    {
+        var user = HttpContext.User;
+        if (user?.Identity.IsAuthenticated == true)
+        {
+            await signInManager.SignOutAsync();
+        }
+
+        return View("LoggedOut");
+    }
+
+}
---
+++ Rsk.Samples.OpenIddict.AdminUI/Models/LoginInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rsk.Samples.OpenIddict.AdminUiIntegration.Models;
+
+public class LoginInputModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    public string ReturnUrl { get; set; }
+    
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
+}
