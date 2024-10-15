132f127b61e7b464e2b5b2802504f47f616a861c
James Britton <james.britton@rocksolidknowledge.com>
2024-10-15 15:15:39 +0100

    feat: migration, script, and apply migration scripts

---
+++ Rsk.Samples.OpenIddict.AdminUI/scripts/apply-migrations.sh
@@ -0,0 +1,6 @@
+dotnet ef database update --context IdentityDbContext
+dotnet ef database update --context ApplicationDbContext
+
+#dotnet ef database update --context SamlConfigurationDbContext
+#dotnet ef database update --context SamlArtifactDbContext
+#dotnet ef database update --context OpenIddictSamlMessageDbContext
